6f475acb00255d824c7884e70d6b640c9fd79fe2
plantfriendzx <froygyg@gmail.com>
2023-09-27 21:06:28 -0400

    trying new slider

---
+++ app/components/Slider.tsx
@@ -0,0 +1,14 @@
+import ReactSlider from "react-slider";
+
+const Slider = () => {
+    return (
+        <ReactSlider 
+        className="slider-bar"
+        trackClassName="slider-track"
+        thumbClassName="slider-thumb"
+        />
+
+    );
+};
+
+export default Slider;
