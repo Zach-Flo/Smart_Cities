fd974b811b07f0aeb30fcdfbcfe3c75fd52b1e84
plantfriendzx <froygyg@gmail.com>
2023-10-01 10:44:43 -0400

    slider n stuff

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -10,7 +10,7 @@ const Slider = () => {
 
     return (
         <>
-        <div className="w-5/6 left-10 bottom-1/2">
+        <div className="w-6/7 left-10 ">
         <ReactSlider 
         className="left-10 mt-14"
         marks
@@ -22,10 +22,10 @@ const Slider = () => {
         renderThumb = {(props, state) => ( 
             <div {...props} 
             style={{ ...props.style, zIndex: 20 }} 
-            className="relative -ml-3 flex flex-col items-center w-8 h-8 -mt-2 outline-none text-[color:var(--whitev)]">
-            <div className="absolute top-0 -ml-3 inline-block px-2 py-1 mb-2 -mt-8 text-xs bg-[color:var(--blackv)] rounded-sm whitespace-nowrap">
+            className="relative flex flex-col items-center w-7 h-7  outline-none text-[color:var(--whitev)]">
+            <div className="absolute top-0 inline-block px-2 py-1 mb-2 -mt-10 text-xs bg-[color:var(--blackv)] rounded-sm whitespace-nowrap">
             {getTime(state.valueNow)}</div>
-            <div className="mt-1 -ml-3 cursor-pointer border-4 bg-[color:var(--whitev)] border-[color:var(--greenv)] w-7 h-7 rounded-full outline-none shadow-lg" />
+            <div className=" cursor-pointer -mt-2 border-4 bg-[color:var(--whitev)] border-[color:var(--greenv)] w-7 h-7 rounded-full outline-none shadow-lg" />
             </div>
             )}
 
