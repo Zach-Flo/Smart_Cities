708280eea97befb831db4f20ad31201081558a80
plantfriendzx <robin.gould@uconn.edu>
2023-10-03 08:55:57 -0400

    12345

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ function App() {
         data: './sample.geojson'
       });
       map.current!.addLayer({
-        id: 'collisions',
+        id: 'trips',
         type: 'circle',
         source: 'map_data',
         paint: {
@@ -63,8 +63,8 @@ function App() {
     <>
     
     <div ref={ mapContainer } className="map-container absolute w-full inset-y-1" />
-    <div id='header' className="absolute grid grid-cols-4 bg-[color:var(--whitev)] w-full h-24 shadow-xl">
-      <div id='tile' className="bg-[color:var(--greenv)] py-3 px-5">
+    <div id='header' className="absolute grid grid-cols-3 bg-[color:var(--whitev)] w-full h-24 shadow-xl">
+      <div id='tile' className="bg-[color:var(--greenv)] py-3 px-5 h-24">
         <h1 className="text-[color:var(--whitev)] text-4xl text-left">E-Scooter Data</h1>
         <p className="text-[color:var(--grayv)] text-xl text-left">Chicago, IL, USA - 2020</p>
       </div> 
