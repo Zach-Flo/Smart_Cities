786818b8f67e837a65eac9c33c7efec509606d3d
Zach Florian <zachary.florian@uconn.edu>
2023-10-13 15:55:50 -0400

    got parralax working

--- app/components/information.tsx
+++ app/components/information.tsx
@@ -1,27 +1,51 @@
 import '../globals.css';
-import {Parallax} from 'react-parallax';
-import FallenScooters from '../images/FallenScooters.jpg';
-import ScooterHub from '../images/ScooterHub.jpg';
-import KidOnScooter from '../images/KidOnScooter.jpg';
+import {Parallax, Background} from 'react-parallax';
+import FallenScooters from '../img/FallenScooters.svg';
+import ScooterHub from '../img/ScooterHub.svg';
+import KidOnScooter from '../img/KidOnScooter.svg';
 import { url } from 'inspector';
 
 const information = () => {
     return (
         <>
-            <Parallax className='fallen-scooter' strength={800}>
-                <div className='content'>
-                    <div className='img-txt'>Exploring E-Scooter Trends: Data Insights</div>
-                </div>
+        
+            <Parallax className='fallen-scooter' bgImage={FallenScooters} strength={100}>
+                <Background className=''>
+                    <div className='content'>
+                        <div className=' img-txt'>Exploring E-Scooter Trends: Data Insights</div>
+                        <img 
+                        src="https://s3-prod.crainsnewyork.com/Screen%20Shot%202023-06-16%20at%201.15.42%20PM.png"
+                        className=' min-h-screen  max-w-screen-2xl' 
+                        alt='scooter'>
+                        </img>
+                    </div>
+                </Background>
+                
             </Parallax>
-            <Parallax className='kid-on-scooter' strength={800}>
-                <div className='content'>
-                    <div className='img-txt'>Exploring E-Scooter Trends: Data Insights</div>
-                </div>
+            <div className='flex bg-white h-48'></div>
+            <Parallax className='kid-on-scooter' strength={100}>
+                <Background className=''>
+                    <div className='content'>
+                        <img 
+                        src="https://npr.brightspotcdn.com/b4/cb/a34546db4848968f50a8139673a7/ap19275506168896.jpg"
+                        className=' min-h-screen  max-w-screen-2xl' 
+                        alt='scooter'>
+                        </img>
+                    </div>
+                </Background>
             </Parallax>
-            <Parallax className='scooter-hub' strength={800}>
-                <div className='content'>
-                    <div className='img-txt'>Exploring E-Scooter Trends: Data Insights</div>
-                </div>
+            <div className='flex bg-white h-48'></div>
+            <Parallax className='scooter-hub' strength={100}>
+                <Background className=''>
+                    <div className='content'>
+                        <div className=' img-txt'>Exploring E-Scooter Trends: Data Insights</div>
+                        <img 
+                        src="https://www.redbrick.me/wp-content/uploads/2021/08/ernest-ojeh-Jmz7CfSRQzI-unsplash-2.jpg"
+                        className=' min-h-screen  max-w-screen-2xl' 
+                        alt='scooter'>
+                        </img>
+                    </div>
+                </Background>
             </Parallax>
         </>
     );
