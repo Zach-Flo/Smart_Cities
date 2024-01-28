fb57dfc2af663a6269a96e784192a62379b7921a
Zach Florian <zachary.florian@uconn.edu>
2024-01-28 13:17:18 -0500

    added linter and linted information.tsx

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -22,7 +22,7 @@ const Slider = () => {
         renderThumb = {(props, state) => ( 
             <div {...props} 
             style={{ ...props.style, zIndex: 20 }} 
-            className="relative flex flex-col items-center w-7 h-7  outline-none text-[color:var(--whitev)]">
+            className='relative flex flex-col items-center w-7 h-7  outline-none text-[color:var(--whitev)]'>
             <div className="absolute top-0 inline-block px-2 py-1 mb-2 -mt-10 text-xs bg-[color:var(--blackv)] rounded-sm whitespace-nowrap">
             {getTime(state.valueNow)}</div>
             <div className=" -mt-2 border-4 bg-[color:var(--whitev)] border-[color:var(--greenv)] w-7 h-7 rounded-full outline-none shadow-lg" />
