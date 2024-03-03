ebee57182b3bb603b1562e223517d317d80cd9cb
Zach Florian <zachary.florian@uconn.edu>
2024-03-02 19:53:44 -0500

    testing something

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -7,6 +7,7 @@ import { loadDataSource, setChoroplethView } from "./map_functions"
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const apiKey = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || "pk.eyJ1IjoiemFjaGZsbyIsImEiOiJjbHQ3cWYyN2MwYnZ3MnFvZGtxMnl0ZmVnIn0.I6mUKSYjwbuH_XX_UAEcTg";
+console.log(apiKey);
 mapboxgl.accessToken = apiKey;
 
 
