fea3c765cd813c7dc5fd5e5953dd095ac932c581
Zach Florian <zachary.florian@uconn.edu>
2024-04-14 15:07:56 -0400

    fixed datepicker

--- app/components/query_preprocessed/MyDatePicker.tsx
+++ app/components/query_preprocessed/MyDatePicker.tsx
@@ -2,18 +2,29 @@ import 'react-datepicker/dist/react-datepicker.css'
 import DatePicker from 'react-datepicker'
 import React, { useState } from 'react'
 
-export default function MyDatePicker( { onDateChange }){
+export default function MyDatePicker({ onDateChange }) {
     const minDate = new Date('2020-11-19');
     const maxDate = new Date('2020-12-13');
     const [startDate, setStartDate] = useState(minDate);
 
-    return(
-        <DatePicker className=' text-center rounded-lg text-xl border-black z-50 font-bold'
-            selected={startDate} 
-            onChange={onDateChange} 
+    const handleDateChange = (date) => {
+        setStartDate(date); // Update the state with the new date
+        if (onDateChange) {
+            onDateChange(date); // Call the onDateChange function with the new date
+        }
+    };
+
+    return (
+        <DatePicker className='text-center rounded-lg text-xl border-black z-50 font-bold'
+            selected={startDate}
+            onChange={handleDateChange}
             minDate={minDate}
             maxDate={maxDate}
             showIcon
+            customTimeInput={false}
+            onKeyDown={(e) => {
+                e.preventDefault();
+            }}
         />
-    )
+    );
 }
